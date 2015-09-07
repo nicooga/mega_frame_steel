62d578c6247d93b16acaa5d0aa6e8f877b067b3c
Nicolas Oga <2112.oga@gmail.com>
2015-09-07 06:38:48 -0300

    FIRST_COMMIT

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name megaFrameSteelApp
+ * @description
+ * # megaFrameSteelApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('megaFrameSteelApp', []);
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name megaFrameSteelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the megaFrameSteelApp
+ */
+angular.module('megaFrameSteelApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
