01e31d9c62c6377ff8381cad7e6e7a4e143e979d
Nicolas Oga <2112.oga@gmail.com>
2015-09-09 05:00:22 -0300

    Add smooth scroll to

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@
   angular
     .module('megaFrameSteelApp', [
       'ngMaterial',
-      'slick'
+      'slick',
+      'smoothScroll'
     ])
 
     .run(function(Mandrill) {
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-material/angular-material.js',
       'bower_components/slick-carousel/slick/slick.min.js',
       'bower_components/angular-slick/dist/slick.js',
+      'bower_components/angular-smooth-scroll/angular-smooth-scroll.js',
       'bower_components/mustache.js/mustache.js',
       'bower_components/slick/dist/slick.js',
       'bower_components/angular-mocks/angular-mocks.js',
